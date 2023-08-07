4ed77a9687dce61a2523d587ab8dfd4f06a189fb
AntoEpr <140263966+AntoEpr@users.noreply.github.com>
2023-08-07 22:05:55 +0300

    all done

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,11 @@
-import { performAction } from './js/app';
-import { getWeatherData} from './js/app';
-import { getWeatherDataByZip} from './js/app';
-import { postData} from './js/app';
-import './styles/style.scss'
+import { performAction } from './js/app'
+import { getWeatherData} from './js/app'
+import { getWeatherDataByZip} from './js/app'
+import { postData} from './js/app'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/footer.scss'
 export{
     performAction,
     getWeatherData,
