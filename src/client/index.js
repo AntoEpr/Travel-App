e9c140d7a24da9b5c56c2efa1b148901227cdfc6
AntoEpr <140263966+AntoEpr@users.noreply.github.com>
2023-08-08 22:19:42 +0300

    all done

--- src/client/index.js
+++ src/client/index.js
@@ -1,16 +1,8 @@
 import { userInput } from './js/formHandler';
-import { performAction } from './js/app'
-import { getWeatherData} from './js/app'
-import { getWeatherDataByZip} from './js/app'
-import { postData} from './js/app'
 import './styles/base.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 import './styles/footer.scss'
 export{
-    performAction,
-    getWeatherData,
-    getWeatherDataByZip,
-    postData,
     userInput,
 }
